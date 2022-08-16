7ad413837f4f6eb42d050501189762180ff1d794
schons <schonsluuiz@gmail.com>
2022-08-16 19:49:35 -0300

    Start studies

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int [] ages = new int[5];
+
+        ages[0] = 20;
+        ages[1] = 30;
+        ages[2] = 40;
+        ages[3] = 50;
+        ages[4] = 60;
+
+        int age1 = ages[0];
+        int age2 = ages[1];
+        int age3 = ages[2];
+        int age4 = ages[3];
+        int age5 = ages[4];
+
+        System.out.println(age5);
+        System.out.println(ages.length);
+    }
+}
